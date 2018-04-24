8947915375beaaa4a4e2ea8428e2e0ee5cb29cba
ekansrm <ekansrm0001@163.com>
2018-04-24 15:14:40 +0800

    尝试修正线上kafka问题

--- src/main/java/ekansrm/exp/storm/wordcount/Topology.java
+++ src/main/java/ekansrm/exp/storm/wordcount/Topology.java
@@ -1,14 +1,9 @@
 package ekansrm.exp.storm.wordcount;
 
 import org.apache.storm.kafka.*;
-import org.apache.storm.redis.bolt.RedisStoreBolt;
-import org.apache.storm.redis.common.config.JedisPoolConfig;
-import org.apache.storm.redis.common.mapper.RedisStoreMapper;
 import org.apache.storm.spout.SchemeAsMultiScheme;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
-
-import java.util.Arrays;
 
 /**
  * @author kami.wm
@@ -36,24 +31,22 @@ public class Topology {
   }
 
   static public TopologyBuilder kafkaWordCountBuilder() {
-    String zks = "storm_kafka_1:9092,storm_kafka_2:9092,storm_kafka_3:9092";
+    String zks = "172.22.0.6:2181,172.22.0.5:2181,172.22.0.9:2181";
     String topic = "storm-word-count";
 
     // default zookeeper root configuration for storm
-    String zkRoot = "/storm";
+    String zkRoot = "";
 
-    String id = "word";
+    String id = "storm-word-count-test";
 
-    BrokerHosts brokerHosts = new ZkHosts(zks);
-    SpoutConfig spoutConf = new SpoutConfig(brokerHosts, topic, zkRoot, id);
-    spoutConf.scheme = new SchemeAsMultiScheme(new StringScheme());
-    spoutConf.zkServers = Arrays.asList("zk1.cloud", "zk2.cloud2", "zk3.cloud");
-    spoutConf.zkPort = 2181;
+    ZkHosts zkHosts = new ZkHosts(zks);
+    SpoutConfig kafkaSpout = new SpoutConfig(zkHosts, topic, zkRoot, id);
+    kafkaSpout.scheme = new SchemeAsMultiScheme(new StringScheme());
 
     TopologyBuilder builder = new TopologyBuilder();
 
     // Kafka我们创建了一个5分区的Topic，这里并行度设置为5
-    builder.setSpout("kafka-reader", new KafkaSpout(spoutConf), 1);
+    builder.setSpout("kafka-reader", new KafkaSpout(kafkaSpout), 1);
     builder.setBolt("word-splitter", new BoltSplitSentence(), 1).shuffleGrouping("kafka-reader");
     builder.setBolt("word-counter", new BoltWordCounter()).fieldsGrouping("word-splitter", new Fields("word"));
 
--- src/main/java/ekansrm/exp/storm/wordcount/TopologySubmitRemote.java
+++ src/main/java/ekansrm/exp/storm/wordcount/TopologySubmitRemote.java
@@ -11,7 +11,10 @@ import org.apache.storm.redis.common.config.JedisPoolConfig;
 import org.apache.storm.redis.common.mapper.RedisStoreMapper;
 import org.apache.storm.topology.TopologyBuilder;
 
+import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  *
@@ -32,9 +35,18 @@ public class TopologySubmitRemote {
 
     //配置nimbus连接端口，默认 6627
     config.put(Config.NIMBUS_THRIFT_PORT, 6627);
+    config.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList("172.22.0.6", "172.22.0.5", "172.22.0.9"));
+    config.put(Config.STORM_ZOOKEEPER_PORT, 2181);
 
-    JedisPoolConfig poolConfig = new JedisPoolConfig.Builder()
-      .setHost("172.22.0.11").setPort(6379).build();
+    Map<String, String> map =new HashMap<>();
+    // 配置Kafka broker地址
+    map.put("metadata.broker.list", "172.22.0.2:9092,172.22.0.3:9092,172.22.0.4:9092");
+    // serializer.class为消息的序列化类
+    map.put("serializer.class", "kafka.serializer.StringEncoder");
+
+    config.put("kafka.broker.properties", map);
+
+    JedisPoolConfig poolConfig = new JedisPoolConfig.Builder().setHost("storm-redis").setPort(6379).build();
     RedisStoreMapper storeMapper = new WordCountStoreMapper();
     RedisStoreBolt storeBolt = new RedisStoreBolt(poolConfig, storeMapper);
 
@@ -46,7 +58,7 @@ public class TopologySubmitRemote {
     // 设置系统属性 storm.jar, 指向jar包的位置
     System.setProperty("storm.jar", "target/exp.storm-1.0-SNAPSHOT.jar");
 
-    StormSubmitter.submitTopology("test", config, builder.createTopology());
+    StormSubmitter.submitTopology("kafka-redis", config, builder.createTopology());
 
   }
 
